a50bc9e596f94303705a50fc9e29e01514661489
mosieczek <ihatebisons@gmail.com>
2020-10-26 16:11:24 +0100

    Initial commit

---
+++ src/main/java/com/example/swozp/SwozpApplication.java
@@ -0,0 +1,13 @@
+package com.example.swozp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwozpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwozpApplication.class, args);
+    }
+
+}
